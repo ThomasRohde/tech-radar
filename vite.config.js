2350096950bcc43ad66681c5986efcea381c23a6
ThomasRohde <thomas@rohde.name>
2024-07-16 07:25:19 +0200

    Vite config import path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import { fileURLToPath } from 'url'
+import path from 'path'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
