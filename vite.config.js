ad4f08322e881c400e6603c7fe88945780ae4caa
ThomasRohde <thomas@rohde.name>
2024-07-16 07:24:01 +0200

    Vite config __dirname

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
